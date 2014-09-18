a06f226454dbfc8961bb685c90f4fb84e140288b
SkullCrusher <boxhead.two.play@gmail.com>
2014-09-18 14:10:09 -0700

    Created Decrypt

--- ProjectMicheal_Encryption.cpp
+++ ProjectMicheal_Encryption.cpp
@@ -1,3 +1,12 @@
+/*
+*	**_ Project Micheal - Encryption and decryption _**
+*
+*	Coded by SkullCrusher for Micheal.
+*
+*	MIT License - Do what you want with it.
+*/
+
+
 #include <iostream>
 #include <Windows.h>
 #include <string>
@@ -30,23 +39,62 @@ void Encrypt(std::string &a, int Seed){
 	}
 }
 
+
+void Decrypt(std::string &a, int Seed){
+
+	//Decrypt all of the string but the first character.
+	for (int i = a.size() - 1; i > 0; i--){
+		char CurrentChar = a[i]; //Grab the current character from the array.
+
+		CurrentChar -= a[i - 1]; //Subtract the value of the character before it.
+
+		//Force it to be in the correct range.
+		while (CurrentChar < 32){
+			CurrentChar += 94;
+		}
+
+		//Update the string.
+		a[i] = CurrentChar;
+	}
+
+	//Grab the first character.
+	char FirstCharacter = a[0];
+
+	//Use the seed as the starting point.
+	FirstCharacter -= Seed;
+
+	//Force it to be in the correct range
+	while (FirstCharacter < 32){
+		FirstCharacter += 94;
+	}
+
+	//Set the character to the updated one.
+	a[0] = FirstCharacter;
+}
+
+
+
 int main(){	
 
 	//https://github.com/SkullCrusher/ProjectMicheal
 
+	//"Lol you can decrypt I see. try the rest."
 	//Encrypted with Seed 23: "ct$D_p)INQa#)05Idv.N9Ynu|Ll$8Ss+5<\\pw.Dr"
 
+	//"SkullCrusher: I hope this works right, I did not test alot."
 	//Encrypted with Seed 43: "~-DR`EYp'18L(H3S]n\")I_it+Kdu+8Mm#.7AW%E0PVag)9J`\"8?Tj,/=Nd4"
 
+	//http://i.imgur.com/4SBSgDo.jpg
 	//Encrypted with Seed 50: "<RhzV'Va1<KTk!OTetEynRGP6Gu#5>"
 
-	//Encrypted with Seed (Unknown): "~*5Jj%(=]dty/J\\ry!AKRfm/:Oor4K_mIis+AS/^/:hs$-DX(->M|mc!TH2c3<GO"
-
-
+	//"This was encrypted here is a url: http://i.imgur.com/OTz3RH1.gif"
+	//Encrypted with Seed 42: "~*5Jj%(=]dty/J\\ry!AKRfm/:Oor4K_mIis+AS/^/:hs$-DX(->M|mc!TH2c3<GO"
+	
 	//An example encryption string.
 	std::string Message = "http://www.cplusplus.com/doc/tutorial/variables/";
 	
-	Encrypt(Message, 23);
+	Encrypt(Message, 17);
+	Decrypt(Message, 17);
 
 	return 0;
 }
